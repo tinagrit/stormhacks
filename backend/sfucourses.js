ac047c58707d3ed35a816d3730212bd842514603
tinagrit <mu@tinagrit.com>
2025-10-05 09:20:56 -0700

    added info to response

--- backend/sfucourses.js
+++ backend/sfucourses.js
@@ -117,6 +117,7 @@ router.get("/outline", async (req, res) => {
   try {
     const data = await cachedFetch(`${BASE_URL}?${encodeURIComponent(year)}/${encodeURIComponent(term)}/${encodeURIComponent(dept)}/${encodeURIComponent(course)}/${encodeURIComponent(section)}`);
     const courseSchedule = data.courseSchedule;
+    const info = data.info;
 
     console.log(courseSchedule);
 
@@ -142,7 +143,7 @@ router.get("/outline", async (req, res) => {
       });
     }
 
-    res.json({ sem: `${term} ${year}`, events});
+    res.json({ sem: `${term} ${year}`, info, events});
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to fetch course outline" });
