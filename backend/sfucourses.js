f80c91ac3bd4f29bee55fca50d780c62c559a7e8
tinagrit <mu@tinagrit.com>
2025-10-05 02:36:15 -0700

    path fix

--- backend/sfucourses.js
+++ backend/sfucourses.js
@@ -4,6 +4,7 @@
 const express = require("express");
 const cors = require("cors");
 const fetch = require("node-fetch");
+const path = require('path');
 
 // -----------------------------------------------------------------------------
 // CONFIGURATION
